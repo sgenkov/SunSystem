f65d13334d3f33500bd4b54aab20e2afb808125c
Stanimir Genkov <s.genkov@wienergames.com>
2021-03-26 17:14:11 +0200

    2D  /  3D easy switch

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import registerServiceWorker from "./registerServiceWorker";
-// import { Application } from 'pixi.js';
-import System from "./3D/System";
+import { Application } from 'pixi.js';
+// import System from "./3D/System";
+import System from './2D/System';
 
-// export const app = new Application({
-//   width: window.innerWidth - 15,
-//   height: window.innerHeight - 25,
-//   backgroundColor: 0x000000,
-// });
-
-// export const system = new System();
-// document.body.appendChild(app.view);
-
+export const app = new Application({
+  width: window.innerWidth - 15,
+  height: window.innerHeight - 25,
+  backgroundColor: 0x000000,
+});
 
 export const system = new System();
+document.body.appendChild(app.view);
+
+
+// export const system = new System(); // 3D 
 
 
 
